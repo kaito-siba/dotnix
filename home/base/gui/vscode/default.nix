76e3e099a7ae605328b2ba2c8d062794667661c2
k-nanchi <kaito@siba-service.jp>
2025-08-04 20:33:03 +0900

    update vscode settings

--- home/base/gui/vscode/default.nix
+++ home/base/gui/vscode/default.nix
@@ -1,9 +1,6 @@
+{ lib, ... }:
 {
   programs.vscode = {
     enable = true;
-
-    extensions = import ./extensions.nix;
-    userSettings = import ./settings.nix;
-    keybindings = import ./keybindings.nix;
   };
 }
