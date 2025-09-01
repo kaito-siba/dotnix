d1cc8dc02286236cbaa798cf9146d11f14f2901b
k-nanchi <kaito@siba-service.jp>
2025-09-01 16:26:01 +0900

    remove static ip settings

--- hosts/siba-ultimate-pc/network.nix
+++ hosts/siba-ultimate-pc/network.nix
@@ -11,9 +11,5 @@
   networking = {
     hostName = "siba-ultimate-pc";
     firewall.allowedTCPPorts = [ 4824 ];
-    interfaces.end0.ipv4.addresses = [ {
-      address = "10.48.6.174";
-      prefixLength = 21;
-    } ];
   };
 } 
