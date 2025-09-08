9555216632ca5896ab531acfd549cb83be9cc3cc
k-nanchi <kaito@siba-service.jp>
2025-09-09 01:18:14 +0900

    skip failing tests on build

--- hosts/radiata/default.nix
+++ hosts/radiata/default.nix
@@ -6,6 +6,24 @@
     ./boot.nix
   ];
 
+  # radiata 専用の overlay: tailscale のテストスキップを追加
+  # https://github.com/tailscale/tailscale/issues/16966
+  nixpkgs.overlays = [
+    (
+      _: prev: {
+        tailscale = prev.tailscale.overrideAttrs (old: {
+          checkFlags =
+            builtins.map (
+              flag:
+                if prev.lib.hasPrefix "-skip=" flag
+                then flag + "|^TestGetList$|^TestIgnoreLocallyBoundPorts$|^TestPoller$"
+                else flag
+            ) old.checkFlags;
+        });
+      }
+    )
+  ];
+
   programs.zsh.enable = true;
   users.users.rkv12 = {
     isNormalUser = true;
