8bf3d8cebe0cd667eac6c1cdac7df666d9d0ccae
k-nanchi <kaito@siba-service.jp>
2025-10-06 08:40:25 +0900

    nvim python settings

--- home/base/base/neovim/config/lua/config/lazy.lua
+++ home/base/base/neovim/config/lua/config/lazy.lua
@@ -18,6 +18,9 @@ require("lazy").setup({
 	spec = {
 		-- add LazyVim and import its plugins
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "catppuccin-mocha" } },
+		-- lazy extra plugins
+		{ import = "lazyvim.plugins.extras.dap.core" },
+		{ import = "lazyvim.plugins.extras.lang.python" },
 		-- import/override with your plugins
 		{ import = "plugins" },
 	},
