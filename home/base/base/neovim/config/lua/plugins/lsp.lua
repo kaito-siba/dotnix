d9183e8714b9324bf3d6ed06b7a7c7b09513cd61
k-nanchi <kaito@siba-service.jp>
2025-11-05 01:03:06 +0900

    nvim setting

---
+++ home/base/base/neovim/config/lua/plugins/lsp.lua
@@ -0,0 +1,73 @@
+-- jsonls schema catalog
+local catalog_data = require("schema-catalog")
+local schemas = catalog_data.schemas
+
+return {
+	-- add pyright to lspconfig
+	{
+		"neovim/nvim-lspconfig",
+		---@class PluginLspOpts
+		opts = {
+			---@type lspconfig.options
+			servers = {
+				-- pyright will be automatically installed with mason and loaded with lspconfig
+				pyright = {},
+				nixd = {},
+			},
+		},
+	},
+
+	-- add tsserver and setup with typescript.nvim instead of lspconfig
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"jose-elias-alvarez/typescript.nvim",
+			init = function()
+				require("lazyvim.util").lsp.on_attach(function(_, buffer)
+          -- stylua: ignore
+          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+					vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+				end)
+			end,
+		},
+		---@class PluginLspOpts
+		opts = {
+			---@type lspconfig.options
+			servers = {
+				-- tsserver will be automatically installed with mason and loaded with lspconfig
+				tsserver = {},
+			},
+			-- you can do any additional lsp server setup here
+			-- return true if you don't want this server to be setup with lspconfig
+			---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+			setup = {
+				-- example to setup with typescript.nvim
+				tsserver = function(_, opts)
+					require("typescript").setup({ server = opts })
+					return true
+				end,
+				-- Specify * to use this function as a fallback for any server
+				-- ["*"] = function(server, opts) end,
+			},
+		},
+	},
+	-- jsonls with schema catalog
+	-- https://zenn.dev/nazo6/articles/989d44e16b1abb
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				jsonls = {},
+			},
+			setup = {
+				jsonls = function(_, opts)
+					opts.settings = {
+						json = {
+							schemas = schemas,
+						},
+					}
+				end,
+			},
+		},
+	},
+}
