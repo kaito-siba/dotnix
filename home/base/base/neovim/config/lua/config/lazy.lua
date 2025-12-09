abe4f279958933caf73f9d0bd6b2f0cfdaa0feb5
k-nanchi <kaito@siba-service.jp>
2025-12-09 11:22:37 +0900

    update nvim config

--- home/base/base/neovim/config/lua/config/lazy.lua
+++ home/base/base/neovim/config/lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
 		{ import = "lazyvim.plugins.extras.ai.copilot" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
+		{ import = "lazyvim.plugins.extras.ai.copilot-chat" },
 		-- import/override with your plugins
 		{ import = "plugins" },
 	},
