2adf568d5edfa11580daf0cbe55b3c69b4c3c988
k-nanchi <kaito@siba-service.jp>
2025-12-15 11:28:23 +0900

    update codex

--- home/base/base/ai/default.nix
+++ home/base/base/ai/default.nix
@@ -2,12 +2,12 @@
 let
   codex-rs = pkgs.stdenv.mkDerivation rec {
     pname = "codex";
-    version = "0.71.0";
+    version = "0.72.0";
 
     src = pkgs.fetchurl {
       url =
         "https://github.com/openai/codex/releases/download/rust-v${version}/codex-x86_64-unknown-linux-musl.tar.gz";
-      sha256 = "sha256-iELx/9Z21FZwBNHoiG8BhjrjKhqoiRZ5Zrjefd2HaiI=";
+      sha256 = "sha256-Qi4D9aXMGjuHc0qlfp1CUBwzRHld9L4t3fmf8f/95xw=";
     };
 
     dontUnpack = true;
