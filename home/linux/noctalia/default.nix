8d8235f496e335b04cab7c1f1cf266a7121ef954
k-nanchi <kaito@siba-service.jp>
2026-01-26 14:42:27 +0900

    add nocalia pomodoro plugin

--- home/linux/noctalia/default.nix
+++ home/linux/noctalia/default.nix
@@ -1,33 +1,40 @@
-{ pkgs, noctalia, ... }:
-{
-  imports = [
-    noctalia.homeModules.default
-  ];
+{ pkgs, noctalia, ... }: {
+  imports = [ noctalia.homeModules.default ];
 
-  home.packages = with pkgs; [
-    gpu-screen-recorder
-    wtype
-    gradia
-  ];
+  home.packages = with pkgs; [ gpu-screen-recorder wtype gradia ];
 
   # configure options
   programs.noctalia-shell = {
     enable = true;
     plugins = {
-      sources = [
-        {
-          enabled = true;
-          name = "Official Noctalia Plugins";
-          url = "https://github.com/noctalia-dev/noctalia-plugins";
-        }
-      ];
+      sources = [{
+        enabled = true;
+        name = "Official Noctalia Plugins";
+        url = "https://github.com/noctalia-dev/noctalia-plugins";
+      }];
       states = {
         screen-recorder = {
           enabled = true;
           sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
         };
+        pomodoro = {
+          enabled = true;
+          sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+        };
       };
       version = 1;
+    };
+
+    pluginSettings = {
+      pomodoro = {
+        workDuration = 25;
+        shortBreakDuration = 5;
+        longBreakDuration = 15;
+        sessionsBeforeLongBreak = 4;
+        autoStartBreaks = true;
+        autoStartWork = true;
+        compactMode = true;
+      };
     };
     # this may also be a string or a path to a JSON file.
   };
