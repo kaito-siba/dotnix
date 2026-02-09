bfe7a6725202ef2b6ae7c833b14a3fbd4403d2b9
k-nanchi <kaito@siba-service.jp>
2026-02-10 07:33:04 +0900

    update radiata network settings

--- hosts/radiata/network.nix
+++ hosts/radiata/network.nix
@@ -3,12 +3,18 @@
     enable = true;
     ports = [ 4824 ];
     settings = {
-        PasswordAuthentication = false;
+      PasswordAuthentication = false;
     };
   };
 
   networking = {
     hostName = "radiata";
     firewall.allowedTCPPorts = [ 4824 ];
+    interfaces.enp4s0 = {
+      wakeOnLan.enable = true;
+    };
+    firewall = {
+      allowedUDPPorts = [ 9 ];
+    };
   };
-} 
+}
