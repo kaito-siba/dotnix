151786fd8ea87e31d7279fddc7c4525becf31bd6
k-nanchi <kaito@siba-service.jp>
2026-02-09 12:00:27 +0900

    update siba network settings

--- hosts/siba-ultimate-pc/network.nix
+++ hosts/siba-ultimate-pc/network.nix
@@ -1,15 +1,17 @@
-{ ... }:
-{
+{ ... }: {
   services.openssh = {
     enable = true;
     ports = [ 4824 ];
     settings = {
-        PasswordAuthentication = false;
+      PasswordAuthentication = false;
+      X11Forwarding = true;
     };
   };
 
   networking = {
     hostName = "siba-ultimate-pc";
     firewall.allowedTCPPorts = [ 4824 ];
+    interfaces.enp129s0 = { wakeOnLan.enable = true; };
+    firewall = { allowedUDPPorts = [ 9 ]; };
   };
-} 
+}
