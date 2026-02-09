21c165e4914e7c802b4d19a28aa7009cdb961538
k-nanchi <kaito@siba-service.jp>
2026-02-09 16:22:21 +0900

    fix delta panic

--- home/base/base/lazygit.nix
+++ home/base/base/lazygit.nix
@@ -7,7 +7,7 @@
         pagers = [
           {
             colorArg = "always";
-            pager = "delta --paging=never";
+            pager = "delta --paging=never --width=120 --max-line-length=2000 --max-syntax-highlighting-length=200";
           }
         ];
       };
