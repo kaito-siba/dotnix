ca1f40fc4e72808272f745cffbbf90f583af613a
k-nanchi <kaito@siba-service.jp>
2026-02-19 16:19:20 +0900

    kitty configuration

--- home/base/base/kitty.nix
+++ home/base/base/kitty.nix
@@ -4,9 +4,14 @@
     enable = true;
     enableGitIntegration = true;
     font = {
-        name = "Guguru Sans Code 35";
+      name = "Maple Mono NF CN";
     };
     shellIntegration.enableZshIntegration = true;
+    settings = {
+      window_margin_width = 10;
+    };
+    extraConfig = ''
+      include ./themes/noctalia.conf
+    '';
   };
 }
-  
