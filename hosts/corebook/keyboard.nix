f770985ba8048723fcb4e9fc7400c2b866af99c1
k-nanchi <kaito@siba-service.jp>
2026-03-09 21:57:57 +0900

    update keymap and trackpad pointer sensitivity

--- hosts/corebook/keyboard.nix
+++ hosts/corebook/keyboard.nix
@@ -11,15 +11,12 @@
       remap = {
         "CapsLock" = "Ctrl_L";
         "Ctrl_L" = "CapsLock";
-      };
-    }
-    {
-      name = "IME switching";
-      # You need to set the action of 'Muhenkan' to 'Deactivate IME' from the mozc settings and remove all Shift + 'Muhenkan' entries.
-      # mozc settings location: fcitx-configtool -> Addons -> Mozc -> Configuration Tool -> Keymap Style
-      remap = {
+        "Super_L" = "Alt_L";
+
+        # You need to set the action of 'Muhenkan' to 'Deactivate IME' from the mozc settings and remove all Shift + 'Muhenkan' entries.
+        # mozc settings location: fcitx-configtool -> Addons -> Mozc -> Configuration Tool -> Keymap Style
         "Alt_L" = {
-          held = "Alt_L";
+          held = "Super_L";
           alone = "Muhenkan";
           alone_timeout_mills = 200;
         };
