e6185055d17533b54667a2b1dd4c171927d89683
k-nanchi <kaito@siba-service.jp>
2026-05-08 07:00:21 +0900

    enable tailscale on corebook

--- hosts/corebook/default.nix
+++ hosts/corebook/default.nix
@@ -6,4 +6,15 @@
     ./hardware-configuration.nix
     ./keyboard.nix
   ];
+
+  services.tailscale = {
+    enable = true;
+    openFirewall = true;
+    useRoutingFeatures = "client";
+    extraUpFlags = [ "--accept-dns=true" ];
+    extraSetFlags = [
+      "--ssh"
+      "--operator=$USER"
+    ];
+  };
 }
