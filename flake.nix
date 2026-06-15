d7ebfe6792ccf402909f494baccbc943a6e70fc4
k-nanchi <kaito@siba-service.jp>
2026-06-15 17:03:02 +0900

    use noctalia v4 (legacy)

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+      url = "github:noctalia-dev/noctalia/legacy-v4";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
