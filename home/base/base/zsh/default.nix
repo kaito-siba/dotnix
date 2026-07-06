3f38daa21adfac6c5c0172db17325d1070e5117e
k-nanchi <kaito@siba-service.jp>
2026-07-06 17:52:59 +0900

    set WAYLAND_DISPLAY in tmux sesion

--- home/base/base/zsh/default.nix
+++ home/base/base/zsh/default.nix
@@ -25,6 +25,20 @@
       bindkey '^F' fzf-file-widget
       zstyle ':completion:*default' menu select=1 interactive
       eval "$(sheldon source)"
+
+      # tmuxのセッション環境からWAYLAND_DISPLAYを再取得し、既存ペインでも
+      # 常に有効な値になるようにする（サーバーがブート時起動で古い値を持つ対策）
+      if [[ -n "$TMUX" ]]; then
+        _tmux_refresh_wayland() {
+          local line
+          line=$(tmux show-environment WAYLAND_DISPLAY 2>/dev/null)
+          if [[ "$line" == WAYLAND_DISPLAY=* ]]; then
+            export WAYLAND_DISPLAY="''${line#WAYLAND_DISPLAY=}"
+          fi
+        }
+        autoload -Uz add-zsh-hook
+        add-zsh-hook precmd _tmux_refresh_wayland
+      fi
     '';
   };
 
